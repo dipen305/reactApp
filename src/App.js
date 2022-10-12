bcfe54f502570d2931bc1d7e9e858265bfcef23e
dipen305 <hdipen@gmail.com>
2022-10-12 18:18:16 +0530

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';  
-class App extends Component{  
+import React from 'react';  
+class App extends React.Component{  
    render(){  
       return(  
          <div>  
